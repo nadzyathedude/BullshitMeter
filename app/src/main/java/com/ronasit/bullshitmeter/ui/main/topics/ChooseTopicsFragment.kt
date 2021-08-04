92dbd6b3d6d8be71c056354fe096b6b7f78f7792
nadzyathedude <im2hoot@gmail.com>
2021-08-04 16:58:01 +0600

    #8: setting color on click

--- app/src/main/java/com/ronasit/bullshitmeter/ui/main/topics/ChooseTopicsFragment.kt
+++ app/src/main/java/com/ronasit/bullshitmeter/ui/main/topics/ChooseTopicsFragment.kt
@@ -17,15 +17,37 @@ class ChooseTopicsFragment : BaseFragment<FragmentChooseTopicsBinding>() {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
         val chipGroup = binding.chipGroup
+
+        val colors = resources.getIntArray(R.array.topics_color)
         viewModel.availableTopics.observe(viewLifecycleOwner, { topics ->
-            topics.forEach {
+            topics.forEach { topics ->
                 val chip = Chip(requireContext())
-                chip.text = it
+                chip.text = topics
                 chip.setTextColor(resources.getColor(R.color.white, context?.theme))
                 chipGroup.addView(chip)
+                chipGroup.setOnCheckedChangeListener { _, chipId ->
+
+                    if (chipId % 6 == 0) {
+                        chip.setChipBackgroundColorResource(colors[0])
+                    }
+                    if (chipId % 5 == 0) {
+                        chip.setChipBackgroundColorResource(colors[1])
+                    }
+                    if (chipId % 4 == 0) {
+                        chip.setChipBackgroundColorResource(colors[2])
+                    }
+                    if (chipId % 3 == 0) {
+                        chip.setChipBackgroundColorResource(colors[3])
+                    }
+                    if (chipId % 2 == 0) {
+                        chip.setChipBackgroundColorResource(colors[4])
+                    }
+
+                }
             }
         })
         viewModel.getTopicsFromServer()
+
     }
 
 }
--- app/src/main/java/com/ronasit/bullshitmeter/ui/main/topics/ChooseTopicsViewModel.kt
+++ app/src/main/java/com/ronasit/bullshitmeter/ui/main/topics/ChooseTopicsViewModel.kt
@@ -1,7 +1,9 @@
 package com.ronasit.bullshitmeter.ui.main.topics
 
+import android.graphics.Color
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.ronasit.bullshitmeter.R
 import com.ronasit.bullshitmeter.data.repository.UserRepository
 import com.ronasit.bullshitmeter.data.store.Categories
 import com.ronasit.bullshitmeter.ui.base.BaseViewModel
@@ -11,23 +13,24 @@ import org.koin.core.component.inject
 
 class ChooseTopicsViewModel : BaseViewModel() {
     val availableTopics = MutableLiveData<Array<String>>()
+    val topicsId = MutableLiveData<Array<Int?>>()
     private val userRepository by inject<UserRepository>()
-
-    fun getTopicsFromServer(){
-        try{
-            viewModelScope.launch(Dispatchers.IO){
-                val topicsList = userRepository.getTopics().data.map { it.name.toString() }.toTypedArray()
-                //val topicsList = userRepository.getTopics().data.map { it.toString() }.toTypedArray()
-            availableTopics.postValue(topicsList)
+    fun getTopicsFromServer() {
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+                val topicsList =
+                    userRepository.getTopics().data.map { it.name.toString() }.toTypedArray()
+                val ids = userRepository.getTopics().data.map { it.id?.toInt() }.toTypedArray()
+                availableTopics.postValue(topicsList)
+                topicsId.postValue(ids)
             }
-        }
-        catch (e: Exception){
+        } catch (e: Exception) {
             exceptionMessage.postValue("Failed getting categories")
         }
 
     }
 
-    fun onBoardingClick(){
+    fun onBoardingClick() {
 
     }
 }
