54bd4a026ffab5a36feacaa9e2d47659994308d5
nadzyathedude <im2hoot@gmail.com>
2021-08-06 17:09:38 +0600

    #8: splash screen animation

--- app/src/main/java/com/ronasit/bullshitmeter/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ronasit/bullshitmeter/ui/splash/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.ronasit.bullshitmeter.ui.splash
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.ronasit.bullshitmeter.R
 import com.ronasit.bullshitmeter.ui.main.MainActivity
@@ -13,8 +17,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-
+        val anim = AnimationUtils.loadAnimation(this, R.anim.splashscreen)
+        val image = findViewById<ImageView>(R.id.image_view_loading)
+        image.startAnimation(anim)
         Timer("timerToStartMainActivity", false).schedule(1000) {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
